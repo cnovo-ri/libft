0b052994156ca34f57a1ab1eeabbed8e304c84d3
Carlos NOVO RIBEIRO <cnovo-ri@student.42.fr>
2016-12-12 17:41:31 +0100

    Libefter

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnovo-ri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/17 15:35:16 by cnovo-ri          #+#    #+#             */
+/*   Updated: 2016/11/22 12:47:13 by cnovo-ri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	while (src[i])
+		i++;
+	str = (char *)malloc(sizeof(*str) * (i + 1));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
